cf567d284520fef9bd976bdc2eea5599104ac6f2
ferrugemjs <ferrugemjavascript@gmail.com>
2016-09-01 02:36:03 +0000

    v0.0.3

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function getALias(p_resource_url){
 		var _tagname;
 		var _trueurl;
 		if(patt_alias.test(p_resource_url)){
-			//has a alias
 			var _urlsplit = p_resource_url.split(' as ');
 			_trueurl = _urlsplit[0];
 			_tagname = _urlsplit[1];
@@ -18,10 +17,6 @@ function getALias(p_resource_url){
 			_tagname = p_resource_url.substring(p_resource_url.lastIndexOf("/")+1,p_resource_url.length);
 		};
 		return {tag:_tagname,url:_trueurl};
-}
-
-function getFunctionNameByAlias(array_modules,tag){
-	return "";
 }
 
 
@@ -43,7 +38,6 @@ module.exports = function(opt) {
 			if(fileName.lastIndexOf('\\') > -1){
 				fileName = fileName.substring(fileName.lastIndexOf('\\')+1,fileName.length-5);
 			};	
-			//console.log(fileName);
 			var tmp_mod_name = "_mod_"+fileName.replace("-","_")+"_"+new Date().getTime();
 			var templateFile = new Buffer(decoder.write(file.contents));
 
@@ -53,20 +47,17 @@ module.exports = function(opt) {
 
 			templateFile = superviews(
 										decoder.write(templateFile)
-										//.replace(/(<template .+args=")(.+?)"/g,'$1$2 _$ferrugemRegister _$ferrugemLoad"')
 										.replace('</require>', '')
 										.replace(/<require from="([^"]*)">/g,function(found,p1){
-											if(p1.lastIndexOf('!') > -1){																								console.log(p1);												
+											if(p1.lastIndexOf('!') > -1){												
 												modules_css_to_import.push(p1);
 											}else{
-												var tagobject = getALias(p1);
-												console.log(tagobject);
+												var tagobject = getALias(p1);												
 												modules_to_import.push(tagobject.url+'.html');
 												modules_alias_to_import.push("_"+tagobject.tag.replace(/-/g,"_")+"_");											
 											}
 											return found;
 										})	
-										//.replace(/<require from="([^"]*)">/g, '<script>_$ferrugemRegister.add("$1");</script>')
 										.replace(/<require from="([^"]*)">/g, '')									
 										.replace(/[\n\t\r]/g," ")
 										.replace(/ (\w*)\.((trigger)|(delegate))="([^"]+)"/g," on$1=\"{$event.preventDefault();$5}\"")
@@ -78,30 +69,20 @@ module.exports = function(opt) {
 
 			if(modules_to_import.length > 0){
 				modules_string = ",'"+modules_to_import.join("','")+"'";
-				//console.log(modules_string);
 			}
-
 			if(modules_to_import.length > 0){
 				modules_alias_string = ","+modules_alias_to_import.join(",");
-				//console.log(modules_alias_string);
 			}
-
 			if(modules_css_to_import.length > 0){
 				modules_css_string = ",'"+modules_css_to_import.join("','")+"'";
-				console.log(modules_css_string);
 			}
-			
-
 			templateFile = templateFile
-							.replace(/define\(\['exports', 'incremental-dom'\], function \(exports, IncrementalDOM\) {/g,"define(['exports', 'incremental-dom','ferrugemjs/generic-component', './"+fileName+"' "+modules_string+modules_css_string+"], function (exports, IncrementalDOM,_generic_component_mod, "+tmp_mod_name+modules_alias_string+") { var _"+tmp_mod_name+"_tmp = Object.keys("+tmp_mod_name+")[0];")
-							//.replace(/exports\.([^ ]+) =/g,'exports.$1 = '+tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.render =')
-							.replace(/exports\.([^ ]+) =/g,tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.refresh ='+' _generic_component_mod.GenericComponent.prototype.refresh;exports.$1 = '+tmp_mod_name+'[_'+tmp_mod_name+'_tmp];  '+tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.render =')
-							//.replace(/elementOpen\(("\w+?-[^"]+")([^)]+)\)/g,'_$ferrugemLoad.load($1$2).content(function(){')
+							.replace(/define\(\['exports', 'incremental-dom'\], function \(exports, IncrementalDOM\) {/g,"define(['exports', 'incremental-dom','ferrugemjs', './"+fileName+"' "+modules_string+modules_css_string+"], function (exports, IncrementalDOM,_generic_component_mod, "+tmp_mod_name+modules_alias_string+") { var _"+tmp_mod_name+"_tmp = Object.keys("+tmp_mod_name+")[0];")
+							.replace(/exports\.([^ ]+) =/g,tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.content ='+' _generic_component_mod.GenericComponent.prototype.content;'+tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.configComponent ='+' _generic_component_mod.GenericComponent.prototype.configComponent;'+tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.refresh ='+' _generic_component_mod.GenericComponent.prototype.refresh;exports.$1 = '+tmp_mod_name+'[_'+tmp_mod_name+'_tmp];  '+tmp_mod_name+'[_'+tmp_mod_name+'_tmp].prototype.render =')
 							.replace(/elementOpen\(("\w+?-[^"]+")([^)]+)\)/g,function(found,$1,$2){
 								var mod_temp_name_tag = '_'+$1.replace(/"/g,"").replace(/-/g,"_")+'_';
 								return ' new '+mod_temp_name_tag+'[Object.keys('+mod_temp_name_tag+')[0]]().configComponent('+$1+''+$2+').content(function(){'
 							})
-							//.replace(/elementOpen\(("\w+?-[^"]+")\)/g,'_$ferrugemLoad.load($1,"nokey",[]).content(function(){')
 							.replace(/elementOpen\(("\w+?-[^"]+")\)/g,function(found,$1){
 								var mod_temp_name_tag = '_'+$1.replace(/"/g,"").replace(/-/g,"_")+'_';
 								return ' new '+mod_temp_name_tag+'[Object.keys('+mod_temp_name_tag+')[0]]().configComponent('+$1+',"nokey",[]).content(function(){'
